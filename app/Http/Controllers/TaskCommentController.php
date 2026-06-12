86f92d45d431272f66e052c670dfbf9780dc1c9a
FelitaZel <felitagazella123@gmail.com>
2026-06-12 09:50:10 +0700

    Menambahkan notifikasi email saat komentar dibuat

--- app/Http/Controllers/TaskCommentController.php
+++ app/Http/Controllers/TaskCommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\TaskComment;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Mail;
 
 class TaskCommentController extends Controller
 {
@@ -28,6 +29,19 @@ class TaskCommentController extends Controller
             'comment' => $data['comment'],
             'attachment_path' => $attachmentPath
         ]);
+
+        $task = $comment->task;
+
+        if ($task && $task->assignee_id && $task->assignee_id !== auth()->id()) {
+            $assignee = \App\Models\User::find($task->assignee_id);
+
+            if ($assignee && $assignee->email) {
+                Mail::raw('Ada komentar baru pada tugas yang menjadi tanggung jawab kamu di SyncFlow.', function ($message) use ($assignee) {
+                    $message->to($assignee->email);
+                    $message->subject('Komentar Baru di SyncFlow');
+                });
+            }
+        }
 
         return response()->json([
             'success' => true,
